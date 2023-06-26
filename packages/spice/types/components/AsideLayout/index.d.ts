15a7aa16bb433fbfa9fc97880843e81f1b389121
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2023-06-26 11:33:56 +0900

    develop

--- packages/spice/types/components/AsideLayout/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const AsideLayout: ({ children, asideChildren, }: {
-    children: React.ReactNode;
-    asideChildren: React.ReactNode;
-}) => import("react/jsx-runtime").JSX.Element;
