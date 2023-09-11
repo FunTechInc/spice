2162a077f3013ed509b1f2921b5fc4d54eb2bab7
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2023-09-11 20:24:21 +0900

    lenis v0.0.27

--- app/_layout/MyLenis/index.tsx
+++ app/_layout/MyLenis/index.tsx
@@ -1,18 +1,76 @@
 "use client";
 
-import { useEffect } from "react";
-import { Lenis, useLenis } from "@studio-freight/react-lenis";
+import { useEffect, useRef } from "react";
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 import { useStore } from "@/app/_context/store";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useWindowResizeObserver } from "@/packages/spice/src";
+
+const option = {
+   duration: 0.6,
+   easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+   orientation: "vertical",
+   gestureOrientation: "vertical",
+   smoothWheel: true,
+   wheelMultiplier: 1,
+   smoothTouch: false,
+   touchMultiplier: 2,
+   infinite: false,
+};
 
 export const MyLenis = ({ children }: { children: React.ReactNode }) => {
+   /*===============================================
+	integrate GSAP & ScrollTrigger
+	===============================================*/
    const lenis = useLenis();
-   const isModalOpen = useStore((state: any) => state.isModalOpen);
+   useEffect(() => {
+      if (!lenis) {
+         return;
+      }
+      gsap.registerPlugin(ScrollTrigger);
+      lenis?.on("scroll", ScrollTrigger.update);
+   }, [lenis]);
+
+   const lenisRef = useRef<any>();
+   useEffect(() => {
+      function update(time: number) {
+         lenisRef.current?.raf(time * 1000);
+      }
+      gsap.ticker.add(update);
+      gsap.ticker.lagSmoothing(0);
+      return () => {
+         gsap.ticker.remove(update);
+      };
+   }, []);
+
+   /*===============================================
+	resize
+	===============================================*/
+   useWindowResizeObserver({
+      callback: () => {
+         if (!lenisRef.current) {
+            return;
+         }
+         lenisRef.current?.resize();
+      },
+      debounce: 50,
+   });
+
+   /*===============================================
+	stop lenis
+	===============================================*/
+   const isModalOpen = useStore((state) => state);
    useEffect(() => {
       if (isModalOpen) {
-         lenis?.stop();
+         lenisRef.current?.stop();
       } else {
-         lenis?.start();
+         lenisRef.current?.start();
       }
-   }, [isModalOpen, lenis]);
-   return <Lenis root>{children}</Lenis>;
+   }, [isModalOpen]);
+   return (
+      <ReactLenis root ref={lenisRef} autoRaf={false} option={option}>
+         {children}
+      </ReactLenis>
+   );
 };
