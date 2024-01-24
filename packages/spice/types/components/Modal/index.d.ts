654e8ec7992214e735165db737cd2a1dbaee205a
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-01-24 17:41:40 +0900

    fix Modal

--- packages/spice/types/components/Modal/index.d.ts
+++ packages/spice/types/components/Modal/index.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="react" />
 type ModalProps = {
     className?: string;
+    /** default is button */
+    tag?: keyof JSX.IntrinsicElements;
     children: React.ReactNode;
     dialog: {
         children: React.ReactNode;
@@ -8,9 +10,9 @@ type ModalProps = {
     };
     /** onOpen,onClose */
     callback?: {
-        onOpen?: (target: Element) => void;
-        onClose?: (target: Element) => void;
+        onOpen?: (dialog: Element) => void;
+        onClose?: (dialog: Element) => void;
     };
 };
-export declare const Modal: ({ children, className, dialog, callback, }: ModalProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Modal: ({ children, tag, className, dialog, callback, }: ModalProps) => import("react/jsx-runtime").JSX.Element;
 export {};
