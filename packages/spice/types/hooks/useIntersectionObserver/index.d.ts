8257151db90a31713cac195b814eca0d8925cfb7
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2024-06-24 17:41:04 +0900

    fix:useIntersection

--- packages/spice/types/hooks/useIntersectionObserver/index.d.ts
+++ packages/spice/types/hooks/useIntersectionObserver/index.d.ts
@@ -1,18 +1,16 @@
 /// <reference types="react" />
 export type IntersectionObserverProps = {
     targetRef: React.RefObject<HTMLElement>;
-    /** default:"0px" */
+    /** default : `0px` */
     rootMargin?: string;
-    /** default:0 */
+    /** default : `0` */
     threshold?: number | number[];
-    /** default:false */
+    /** default : `false` */
     once?: boolean;
     dependencies?: any[];
 };
 export type UseIntersectionObserverProps = {
-    callback: {
-        onEnter?: (entry: IntersectionObserverEntry) => void;
-        onLeave?: (entry: IntersectionObserverEntry) => void;
-    };
+    onEnter?: (entry: IntersectionObserverEntry) => void;
+    onLeave?: (entry: IntersectionObserverEntry) => void;
 } & IntersectionObserverProps;
-export declare const useIntersectionObserver: ({ targetRef, rootMargin, threshold, once, callback, dependencies, }: UseIntersectionObserverProps) => void;
+export declare const useIntersectionObserver: ({ targetRef, rootMargin, threshold, once, onEnter, onLeave, dependencies, }: UseIntersectionObserverProps) => void;
