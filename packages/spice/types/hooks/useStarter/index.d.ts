9a58297c6a3dfbc2a233a5c2cb50e499005bd834
takuma-hmng8 <t.hashimoto@funtech.inc>
2024-07-01 19:59:57 +0900

    v1.2.34

--- packages/spice/types/hooks/useStarter/index.d.ts
+++ packages/spice/types/hooks/useStarter/index.d.ts
@@ -1,12 +1,13 @@
 type UseStarterProps = {
     /** Reload when crossing this threshold */
     reloadThresholds?: number[];
-    /** If set to true, "width=360" will be set in "meta[name="viewport"]" , default:true */
+    /** If set to true, "width=360" will be set in "meta[name="viewport"]" , default : `true` */
     isFixViewportForSmall?: boolean;
+    /** default : `false` */
     areYouFun?: boolean;
 };
 /**
  * When page rendering is complete, data-set "spice-rendered" to the body tag.
  */
-export declare const useStarter: ({ reloadThresholds, isFixViewportForSmall, areYouFun, }: UseStarterProps) => void;
+export declare const useStarter: ({ reloadThresholds, isFixViewportForSmall, areYouFun, }?: UseStarterProps) => void;
 export {};
