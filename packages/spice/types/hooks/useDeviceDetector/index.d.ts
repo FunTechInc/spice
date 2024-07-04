505e35ed8091fba7cc033f638d1e028ab35dc059
takuma-hmng8 <t.hashimoto@funtech.inc>
2024-07-04 21:17:52 +0900

    fix:type

--- packages/spice/types/hooks/useDeviceDetector/index.d.ts
+++ packages/spice/types/hooks/useDeviceDetector/index.d.ts
@@ -1,6 +1,8 @@
 type DeviceType = "iOS" | "Android" | undefined;
 export declare const useDeviceDetector: () => {
+    /** `iOS` | `Android` | `undefined` */
     device: DeviceType;
+    /** whether the device is `iOS` || `Android` */
     isMobile: boolean;
 };
 export {};
