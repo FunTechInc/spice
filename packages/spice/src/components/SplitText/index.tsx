b5ba79ecc5cb1254c37b4e36b0c5d90f0ab31600
takuma-hmng8 <t.hashimoto@funtech.inc>
2024-09-03 18:21:41 +0900

    update:SplitText

--- packages/spice/src/components/SplitText/index.tsx
+++ packages/spice/src/components/SplitText/index.tsx
@@ -22,35 +22,51 @@ export const SplitText = ({
 
    const wrappedText = useMemo(
       () =>
-         text.split("\n").flatMap((segment, i, arr) => [
-            ...segment.split(splitTag).map((char, charI) => {
-               if (exception) {
-                  const match = exception.find(
-                     (item) => item.selector === char
-                  );
-                  if (match) {
-                     return (
-                        <span
-                           key={`${i}-${charI}`}
-                           {...rest}
-                           {...match.attributes}>
-                           {char}
-                        </span>
+         text.split(/(\n|###br\.[^#]+###|###br###)/).flatMap((segment, i) => {
+            if (segment === "\n" || segment === "###br###") {
+               return [null, <br key={i} />];
+            }
+            if (segment?.match(/###br\.(.*?)###/)) {
+               return [
+                  null,
+                  <br
+                     key={i}
+                     className={
+                        segment.match(/(?<=###br\.).+?(?=###)/)?.[0] || ""
+                     }
+                  />,
+               ];
+            }
+            return [
+               ...segment.split(splitTag).map((char, charI) => {
+                  if (exception) {
+                     const match = exception.find(
+                        (item) => item.selector === char
                      );
+                     if (match) {
+                        return (
+                           <span
+                              key={`${i}-${charI}`}
+                              {...rest}
+                              {...match.attributes}>
+                              {char}
+                           </span>
+                        );
+                     }
                   }
-               }
-               return char === " " ? (
-                  <span key={`${i}-${charI}`} {...rest}>
-                     &nbsp;
-                  </span>
-               ) : (
-                  <span key={`${i}-${charI}`} {...rest}>
-                     {char}
-                  </span>
-               );
-            }),
-            i < arr.length - 1 ? <br key={i} /> : null,
-         ]),
+                  return char === " " ? (
+                     <span key={`${i}-${charI}`} {...rest}>
+                        &nbsp;
+                     </span>
+                  ) : (
+                     <span key={`${i}-${charI}`} {...rest}>
+                        {char}
+                     </span>
+                  );
+               }),
+               null,
+            ];
+         }),
       [splitTag, text, rest, exception]
    );
 
