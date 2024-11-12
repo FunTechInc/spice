6a2fad8b3fd6a63e07a1938b25543ed00803b925
takuma-hmng8 <t.hashimoto@funtech.inc>
2024-11-12 12:15:02 +0900

    update

--- packages/spice/types/components/CustomBreakLineParser/index.d.ts
+++ packages/spice/types/components/CustomBreakLineParser/index.d.ts
@@ -8,16 +8,12 @@ export declare const CustomBreakLineUtils: {
     getClassName: (line: string) => string;
 };
 /**
- * Parses a string and formats it by converting specific markers into HTML line breaks.
- * @param {string} content - The input string to be parsed and formatted.
- *                           Use `\n` or `###br###` for regular line breaks,
- *                           and `###br.className###` for a line break with a specific class.
  * @returns {(string | JSX.Element)[]} - A React fragment containing the formatted string with line breaks represented as `<br>` elements.
  */
 declare const useParseBreakLine: (text: string) => (string | JSX.Element)[];
 /**
  * Converts specific markers in a string into HTML line breaks for React components.
- * @param {string} content - The string to format; use \n or ###br.className### for line breaks
+ * @param {string} text - The input string to be parsed and formatted. Use `\n` or `###br###` for regular line breaks, and `###br.className###` for a line break with a specific class.
  * @returns {React.ReactElement} - The formatted string
  */
 declare const CustomBreakLineParser: ({ text, }: CustomBreakLineParserProps) => React.ReactElement;
